dd784b62f7501612de5982917337e2c31c52e576
imsikun <imsikun16@gmail.com>
2021-12-13 15:14:48 +0530

    Recent post implemented

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,5 +1,52 @@
-function PostWidget() {
-  return <div>postwidget</div>;
+import { useState, useEffect } from 'react';
+import moment from 'moment';
+import Link from 'next/link';
+import { getRecentPost, getSimilarPost } from '../services';
+
+function PostWidget({ categories, slug }) {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPost(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      );
+    } else {
+      getRecentPost().then((result) => setRelatedPosts(result));
+    }
+    // return () => {
+    //   cleanup;
+    // };
+  }, [slug]);
+
+  console.log(relatedPosts);
+
+  return (
+    <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+        {slug ? 'Related Posts' : 'Recent Posts'}
+      </h3>
+      {relatedPosts.map((relPost) => (
+        <div key={relPost.title} className='flex items-center w-full mb-4'>
+          <div className='w-16 flex-none'>
+            <img
+              src={relPost.featuredImage.url}
+              alt={relPost.title}
+              height={'60px'}
+              width={'60px'}
+              className='align-middle rounded-full'
+            />
+          </div>
+          <div className='flex-grow ml-4'>
+            <p className='text-gray-500 font-xs'>
+              {moment(relPost.createdAt).format('MMM DD, YYYY')}
+            </p>
+            <Link href={`/post/${relPost.slug}`}>{relPost.title}</Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default PostWidget;
--- services/index.js
+++ services/index.js
@@ -36,3 +36,43 @@ export const getPosts = async () => {
 
   return result.postsConnection.edges;
 };
+
+export const getRecentPost = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(orderBy: createdAt_ASC, last: 3) {
+        title
+        createdAt
+        featuredImage {
+          url
+        }
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.posts;
+};
+
+export const getSimilarPost = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: "$slug"
+          AND: { categories_some: { slug_in: "$categories" } }
+        }
+        lat: 3
+      ) {
+        title
+        createdAt
+        featuredImage {
+          url
+        }
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.posts;
+};
