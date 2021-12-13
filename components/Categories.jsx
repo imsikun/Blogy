302748cbd6b1590cfc7f922ed65b21efc3f98edf
imsikun <imsikun16@gmail.com>
2021-12-13 13:30:30 +0530

    header, postdata fetch done

---
+++ components/Categories.jsx
@@ -0,0 +1,5 @@
+function Categories() {
+  return <div>categories</div>;
+}
+
+export default Categories;
---
+++ components/PostWidget.jsx
@@ -0,0 +1,5 @@
+function PostWidget() {
+  return <div>postwidget</div>;
+}
+
+export default PostWidget;
