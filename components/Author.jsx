fd0c615f63c6673f9b6760b217061bbadfb7f783
imsikun <imsikun16@gmail.com>
2021-12-14 10:26:09 +0530

    single post author and value complete

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
-
-function Author() {
+import Image from 'next/image';
+function Author({ author }) {
   return (
-    <div>
-      <h1>Author</h1>
+    <div class='max-w-sm w-full lg:max-w-full lg:flex'>
+      <div class='border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 leading-normal shadow-lg'>
+        <div class='flex items-center'>
+          <Image
+            unoptimized
+            src={author.photo.url}
+            alt={author.name}
+            height={'100px'}
+            width={'100px'}
+            className='align-middle rounded-full'
+          />
+          <div class='text-sm ml-8'>
+            <p class='text-gray-900 leading-none text-gray-700 text-xl mb-3'>
+              {author.name}
+            </p>
+            <p class='text-gray-600'>{author.bio}</p>
+          </div>
+        </div>
+      </div>
     </div>
+
+    // <div className='text-center mt-20 mb-8 pb-12 rounded-lg bg-black bg-opacity-20 relative'>
+    //   <div className='absolute right-0 left-0 -top-14 '>
+    //     <Image
+    //       unoptimized
+    //       src={author.photo.url}
+    //       alt={author.name}
+    //       height={'100px'}
+    //       width={'100px'}
+    //       className='align-middle rounded-full'
+    //     />
+    //   </div>
+    //   <h3 className='text-white my- text-xl font-bold'>{author.name}</h3>
+    //   <p className='text-white text-lg'>{author.bio}</p>
+    // </div>
   );
 }
 
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -16,7 +16,7 @@ function PostDetails({ post }) {
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='col-span-1 lg:col-span-8'>
           <PostDetail post={post} />
-          <Author post={post.author} />
+          <Author author={post.author} />
           <CommentsForm slug={post.slug} />
           <Comments slug={post.slug} />
         </div>
